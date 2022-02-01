f4e186f6ba29e4b606f3d8c3a7057f3f07cb7c87
unknown <mehakgoel2723@gmail.com>
2022-02-01 16:11:47 +0530

    xdjcs

---
+++ src/main/java/com/flipkart/dropwizard/App.java
@@ -0,0 +1,42 @@
+package com.flipkart.dropwizard;
+
+/**
+ * Hello world!
+ *
+ */
+
+import io.dropwizard.Application;
+import io.dropwizard.Configuration;
+import io.dropwizard.setup.Bootstrap;
+import io.dropwizard.setup.Environment;
+ 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+import com.flipkart.restController.CustomerController;
+import com.flipkart.restController.HelloController;
+
+
+/**
+ * Hello world!
+ *
+ */
+public class App extends Application<Configuration> {
+    private static final Logger LOGGER = LoggerFactory.getLogger(App.class);
+ 
+    @Override
+    public void initialize(Bootstrap<Configuration> b) {
+    }
+ 
+    @Override
+    public void run(Configuration c, Environment e) throws Exception {
+        LOGGER.info("Registering REST resources");
+      //  e.jersey().register(new EmployeeRESTController(e.getValidator()));
+        e.jersey().register(new HelloController());
+        e.jersey().register(new CustomerController());
+    }
+ 
+    public static void main(String[] args) throws Exception {
+        new App().run(args);
+    }
+}
